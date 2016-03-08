9544888f66ff449ecc0a0bbf1e08da672c4b6c93
totliangb <totliangb@gmail.com>
2016-03-08 18:17:08 +0800

    自营物流、自营物流［加急］两种配送方式 添加

---
+++ includes/modules/shipping/benxi_fast.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * ECSHOP 本溪自营物流 插件
+ * ============================================================================
+ * $Author: totliangb $
+ * $Email: totliangb@gmail.com $
+ */
+
+if (!defined('IN_ECS'))
+{
+    die('Hacking attempt');
+}
+
+$shipping_lang = ROOT_PATH.'languages/' .$GLOBALS['_CFG']['lang']. '/shipping/benxi_fast.php';
+if (file_exists($shipping_lang))
+{
+    global $_LANG;
+    include_once($shipping_lang);
+}
+
+include_once(ROOT_PATH . 'languages/' . $GLOBALS['_CFG']['lang'] . '/admin/shipping.php');
+
+/* 模块的基本信息 */
+if (isset($set_modules) && $set_modules == TRUE)
+{
+    $i = (isset($modules)) ? count($modules) : 0;
+
+    /* 配送方式插件的代码必须和文件名保持一致 */
+    $modules[$i]['code']    = basename(__FILE__, '.php');
+
+    $modules[$i]['version'] = '1.0.0';
+
+    /* 配送方式的描述 */
+    $modules[$i]['desc']    = 'benxi_fast_desc';
+
+    /* 配送方式是否支持货到付款 */
+    $modules[$i]['cod']     = false;
+
+    /* 插件的作者 */
+    $modules[$i]['author']  = 'totliangb';
+
+    /* 插件作者的官方网站 */
+    $modules[$i]['website'] = 'http://totliangb.github.io';
+
+    
+    /* 配送接口需要的参数 */
+    $modules[$i]['configure'] = array(
+                                   array('name' => 'base_fee',     'value'=>100),
+                                );
+
+    /* 模式编辑器 */
+    $modules[$i]['print_model'] = 2;
+
+    /* 打印单背景 */
+    $modules[$i]['print_bg'] = '';
+
+   /* 打印快递单标签位置信息 */
+    $modules[$i]['config_lable'] = '';
+
+    return;
+}
+
+/**
+ * 本溪自营物流
+ *
+ */
+class benxi_fast
+{
+
+    /**
+     * 配置信息
+     */
+    var $configure;
+
+    /**
+     * 构造函数
+     *
+     * @param: $configure[array]    配送方式的参数的数组
+     *
+     * @return null
+     */
+    function benxi_fast($cfg=array())
+    {
+        foreach ($cfg AS $key=>$val)
+        {
+            $this->configure[$val['name']] = $val['value'];
+        }
+    }
+
+    /**
+     * 计算订单的配送费用的函数
+     *
+     * @param   float   $goods_weight   商品重量
+     * @param   float   $goods_amount   商品金额
+     * @param   float   $goods_number   商品件数
+     * @return  decimal
+     */
+    function calculate($goods_weight, $goods_amount, $goods_number)
+    {
+        if ($this->configure['free_money'] > 0 && $goods_amount >= $this->configure['free_money'])
+        {
+            return 0;
+        }
+        else
+        {
+            return $this->configure['base_fee'];
+        }
+    }
+
+    /**
+     * 查询发货状态
+     * 该配送方式不支持查询发货状态
+     *
+     * @access  public
+     * @param   string  $invoice_sn     发货单号
+     * @return  string
+     */
+    function query($invoice_sn)
+    {
+        return $invoice_sn;
+    }
+    
+}
+
+?>
